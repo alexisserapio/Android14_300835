dc31b1a57187b686e36d35a149fffaa12eb63700
Alexis Serapio <alexis.serapioh@gmail.com>
2025-09-05 16:34:52 -0600

    Se añade el compose del dia 050925

--- app/src/main/java/com/example/android14/practicas/compose/ComposeFormsActivity.kt
+++ app/src/main/java/com/example/android14/practicas/compose/ComposeFormsActivity.kt
@@ -3,6 +3,23 @@ package com.example.android14.practicas.compose
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.android14.R
@@ -17,5 +34,40 @@ class ComposeFormsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val composeView = findViewById<ComposeView>(R.id.formView)
+        composeView.setContent {
+            FormComposable()
+        }
     }
+}
+
+@Composable
+fun FormComposable(){
+    var name by rememberSaveable { mutableStateOf("") }
+    var email by rememberSaveable { mutableStateOf("") }
+    var isChecked by rememberSaveable { mutableStateOf(false) }
+    var selectedOption by rememberSaveable { mutableStateOf("") }
+
+    val isFormValid = name.isNotBlank()&&email.isNotBlank()&& isChecked && selectedOption.isNotBlank()
+
+    Box(modifier = Modifier.fillMaxSize()
+        .padding(16.dp),
+        contentAlignment = Alignment.Center){
+
+        Card(modifier = Modifier
+            .fillMaxSize(),
+            elevation = CardDefaults.cardElevation(8.dp),
+            shape = RoundedCornerShape(16.dp)
+        ){
+
+        }
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FormPreview(){
+    FormComposable()
 }
